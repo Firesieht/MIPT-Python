8f5395eb1e04c77522cf8882746fef7d5002b385
= <=>
2024-12-08 14:54:35 +0300

    delete cache

--- culture/models.py
+++ culture/models.py
@@ -3,6 +3,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from session1.models import Exhibits, ExhibitOwnerProxy, Organization, Exhibition, Prostranstvo
 from django.core.exceptions import ValidationError
+from django.db.models.signals import ModelSignal
+from typing import Any
 
 
 class ExhibitProxy(Exhibits):
@@ -17,7 +19,7 @@ class OrganizationProxy(Organization):
         verbose_name = 'Организация'
         verbose_name_plural = 'Организации'
     
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -37,12 +39,9 @@ class OrderToCreateExhibition(models.Model):
     class Meta:
         verbose_name = 'Приказ о проведении выставки'
         verbose_name_plural = 'Приказы о проведении выставок'
-    
-    # def clean(self):
-    #     if self.exhibition.type == 'Внешняя':
 
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'Приказ о проведении ({self.date_created}, {self.exhibition})'
 
 
@@ -50,10 +49,10 @@ class OrderExhibitionFromAuthors(models.Model):
     date_getting = models.DateTimeField('Дата получения экспонатов')
     creation_order = models.ForeignKey(OrderToCreateExhibition, verbose_name='Приказ о проведении выставки', on_delete=models.CASCADE)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'Приказ о получении экспонатов: {self.creation_order}'
 
-    def clean(self):
+    def clean(self) -> None:
         if not self.creation_order.exhibition.ex_type == 'Внешняя':
             raise ValidationError('Вам не нужно заполнять этот приказ')
         
@@ -67,10 +66,10 @@ class OrderExhibitionToExhibit(models.Model):
     date_to_provide = models.DateTimeField('Дата передачи')
     order_to_create_exhibit = models.ForeignKey(OrderToCreateExhibition, verbose_name='Приказ о проведении выставки', on_delete=models.CASCADE)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'Акт передачи экспонатов на выставку ({self.order_to_create_exhibit})'
 
-    def clean(self):
+    def clean(self) -> None:
         for exhibit in self.order_to_create_exhibit.exhibitauthormapping_set.all():
             fuck = len(ExhibitAuthorMapping.objects.filter(exhibit=exhibit.exhibit, order_to_get__isnull=False))
             fuck1 = len(ExhibitAuthorMapping.objects.filter(exhibit=exhibit.exhibit, order_to_return__isnull=False))
@@ -87,7 +86,7 @@ class OrderExhibitionToExhibit(models.Model):
 
     class Meta:
         verbose_name = 'Акт передачи экспонатов на выставку'
-        verbose_name_plural = 'Акты передачи экспонатов на выставку'
+        verbose_name_plural = 'Акты передачи ��кспонатов на выставку'
 
 
 class OrderToReturn(models.Model):
@@ -98,12 +97,12 @@ class OrderToReturn(models.Model):
         verbose_name = 'Акт возврата экспонатов'
         verbose_name_plural = 'Акты возврата экспонатов'
     
-    def clean(self):
+    def clean(self) -> None:
         if self.order_to_create.exhibition.ex_type == 'Внешняя':
                 if not len(self.order_to_create.orderexhibitionfromauthors_set.all()):
                     raise ValidationError('Нет акта приема передачи')
     
-    def __str__(self):
+    def __str__(self) -> str:
         return f'Акт возврата экспонатов: {self.order_to_create}'
 
 
@@ -120,19 +119,21 @@ class ExhibitAuthorMapping(models.Model):
         verbose_name_plural = 'Маршруты'
     
 
+
 @receiver(post_save, sender=OrganizationProxy)
-def organization(sender, instance, *args, **kwargs):
+def organization(sender: type, instance: OrganizationProxy, **kwargs: Any) -> None:
     ExhibitOwnerProxy.objects.create(org=instance)
 
+
 @receiver(post_save, sender=ExhibitAuthorMapping)
-def author_mapping(sender, instance, created, *args, **kwargs):
+def author_mapping(sender: type, instance: ExhibitAuthorMapping, created: bool, **kwargs: Any) -> None:
     if created:
         instance.owner = str(instance.exhibit.owner)
         instance.save()
 
 
 @receiver(post_save, sender=OrderExhibitionToExhibit)
-def get_exhibits(sender, instance, created, *args, **kwargs):
+def get_exhibits(sender: type, instance: OrderExhibitionToExhibit, created: bool, **kwargs: Any) -> None:
     if created:
         for exhibit in instance.order_to_create_exhibit.exhibitauthormapping_set.all():
             exhibit.order_to_provide = instance
@@ -140,21 +141,23 @@ def get_exhibits(sender, instance, created, *args, **kwargs):
 
 
 @receiver(post_save, sender=OrderExhibitionFromAuthors)
-def get_exhibits_(sender, instance, created, *args, **kwargs):
+def get_exhibits_(sender: type, instance: OrderExhibitionFromAuthors, created: bool, **kwargs: Any) -> None:
     if created:
         for exhibit in instance.creation_order.exhibitauthormapping_set.all():
             exhibit.order_to_get = instance
             exhibit.save()
 
+
 @receiver(post_save, sender=OrderToReturn)
-def get_exhibits_(sender, instance, created, *args, **kwargs):
+def get_exhibits_return(sender: type, instance: OrderToReturn, created: bool, **kwargs: Any) -> None:
     if created:
         for exhibit in instance.order_to_create.exhibitauthormapping_set.all():
             exhibit.order_to_return = instance
             exhibit.save()
 
+
 @receiver(post_save, sender=ExhibitAuthorMapping)
-def fuck3(sender, instance, created, *args, **kwargs):
+def check_exhibit_owner(sender: type, instance: ExhibitAuthorMapping, created: bool, **kwargs: Any) -> None:
     self = instance
     if self.order_to_create.exhibition.ex_type == 'Внешняя':
         if not self.exhibit.owner.org:
